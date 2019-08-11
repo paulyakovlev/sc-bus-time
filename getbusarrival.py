b6b679cfa365fa1070e4aa93e2c3bdb7a22d4aee
Paul Yakovlev <yvpavel@gmail.com>
2019-08-10 23:54:04 -0700

    handle case when schedule is empty

--- getbusarrival.py
+++ getbusarrival.py
@@ -10,36 +10,42 @@ def get_bus_schedule():
     r = requests.get(URL)
 
     soup = BeautifulSoup(r.content, 'html5lib')
-
     table = soup.find('table', attrs={'class': 'metrotable'})
 
-    bus_times_table = pd.DataFrame(columns=range(
-        0, 6), index=range(0, 20))  # I know the size
+    bus_times_table = pd.DataFrame(columns=range(0, 6), index=range(0, 20))
 
     row_marker = 0
-    for row in table.find_all('tr'):
-        column_marker = 0
-        columns = row.find_all('td')
-        for column in columns:
-            bus_times_table.iat[row_marker, column_marker] = column.get_text()
-            column_marker += 1
-        row_marker += 1
 
-    return(bus_times_table)
+    try:
+        for row in table.find_all('tr'):
+            column_marker = 0
+            columns = row.find_all('td')
+            for column in columns:
+                bus_times_table.iat[row_marker,
+                                    column_marker] = column.get_text()
+                column_marker += 1
+            row_marker += 1
+        return(bus_times_table)
+
+    except AttributeError:
+        raise
 
 
 def time_until_next_bus():
-    table = get_bus_schedule()
-    time = table.iat[1, 1]
-    s1 = datetime.strptime(time, '%I:%M%p')
-    s2 = datetime.now()
-    FMT = '%H:%M:%S'
-    tdelta = s2 - s1
-    print(tdelta)
+    try:
+        table = get_bus_schedule()
+        time = table.iat[1, 1]
+        s1 = datetime.strptime(time, '%I:%M%p')
+        s2 = datetime.now()
+        FMT = '%H:%M:%S'
+        tdelta = s2 - s1
+        return(tdelta)
+    except AttributeError:
+        return "No buses running"
 
 
 def main():
-    print("hello")
+    print(time_until_next_bus())
 
 
 if __name__ == "__main__":
